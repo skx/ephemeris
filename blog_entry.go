18c9349613a0679ee453b903439aee61aee0621b
Steve Kemp <steve@steve.org.uk>
2019-10-12 12:27:01 +0300

    Added test-coverage for blog_entry.

--- blog_entry.go
+++ blog_entry.go
@@ -119,10 +119,7 @@ func NewBlogEntry(path string, site *Ephemeris) (BlogEntry, error) {
 	//
 	// Make a Regex to say we only want letters and numbers
 	//
-	reg, err := regexp.Compile("[^a-zA-Z0-9]")
-	if err != nil {
-		return result, err
-	}
+	reg := regexp.MustCompile("[^a-zA-Z0-9]")
 
 	//
 	// Normalise the output
